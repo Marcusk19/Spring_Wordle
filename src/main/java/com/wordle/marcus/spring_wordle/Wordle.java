709d5854741705e93e2195952351780d9a26be00
Marcus Kok <kokm@kokm-a01.vmware.com>
2022-02-09 15:24:32 -0500

    Words now pulled from https://raw.githubusercontent.com/charlesreid1/five-letter-words/master/sgb-words.txt

--- src/main/java/com/wordle/marcus/spring_wordle/Wordle.java
+++ src/main/java/com/wordle/marcus/spring_wordle/Wordle.java
@@ -1,6 +1,8 @@
 package com.wordle.marcus.spring_wordle;
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.*;
 
 
@@ -8,17 +10,24 @@ public class Wordle {
     List <String> words = new ArrayList<String>();
     private String solution = "marky";
     // private String guess = "";
-
+    URL url;
+    String username = "user";
+    String password = "passwd";
     public Wordle() {
-        BufferedReader reader;
+ 
         try { 
+            url = new URL("https://raw.githubusercontent.com/charlesreid1/five-letter-words/master/sgb-words.txt");
+            URLConnection uc = url.openConnection();
+            uc.setRequestProperty("X-Requested-With", "Curl");
+            String userpass = username + ":" + password;
+            String basicAuth = "Basic" + new String(Base64.getEncoder().encode(userpass.getBytes()));
+            uc.setRequestProperty("Authorization", basicAuth);
 
-            reader = new BufferedReader(new FileReader("words.txt"));
-            String line = reader.readLine();
-            while(line != null) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+            String line = null;
+            while ((line = reader.readLine()) != null) 
                 words.add(line);
-                line = reader.readLine();
-            }
+            
             reader.close();
         } catch(Exception e) {
             e.printStackTrace();
